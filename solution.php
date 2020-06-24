6938fa70b3ea2e6f0d7634043ead688edc222a46
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-06-24 12:22:18 -0300

    refactor

--- solution.php
+++ solution.php
@@ -2,6 +2,7 @@
 /*
 Rotate an array to the right by a given number of steps.
 */
+
 /**
  * @param array $A
  * @param int $K
@@ -11,29 +12,12 @@ function solution($A, $K): array
 {
     $B =[];
     $len = count($A);
-
     if($K === 0 || $K === $len){
         return $A;
-    }elseif ($K < $len){
-        foreach ($A as $key => $a){
-            if($key+$K < $len)
-                $B[$key+$K] = $a;
-            else{
-                $B[($key+$K)-$len] = $a;
-            }
-        }
-        ksort($B);
-        return $B;
     }
 
     foreach ($A as $key => $a){
-
-        if($key-($len-$K) < $len){
-            $B[$key-($len-$K)] = $a;
-        }else{
-            //
-            $B[(($K+$key)%$len)] = $a;
-        }
+        $B[(($K+$key)%$len)] = $a;
     }
     ksort($B);
     return $B;
